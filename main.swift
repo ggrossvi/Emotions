62002da54ba3e12b1dbe0f544d991be68fc9e58b
Gloria V <hello.gloria@gmail.com>
2022-11-13 03:10:18 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,52 @@
+func getName () { 
+    print("Hello, What is your name?")
+    //let name: String
+    if let name = readLine(){
+        print(name)
+    }    
+}
+/*
+ var userName: String
+ userName = getName()
+ print("userName: \(userName)")
+
+
+ func greeting (userName: String) { 
+     
+    print("\(userName) are you feeling happy, sad, frustrated or blah?")
+    if var emotion = readLine(){
+        print("emotion: \(emotion)"
+    }}
+   /
+
+    
+var quotes = [[String]]()
+
+ var happyQuotes = [
+        
+    ]
+
+var sadQuotes" = [
+        
+    ]
+*/
+/*
+
+var frustratedQuotes = [
+        "Don't focus on the pain, focus on the progress."   
+    ]
+
+    "blah":[
+        
+    ]
+quotes.append(frustratedQuotes)
+if (emotion == "frustrated") {  
+    print(frustratedQuotes[0])
+}
+*/
+
+getName()
+//print(getName())
+
+
+  
